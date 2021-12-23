588727f153f8570c29ca6fb7f66be5344f6bf96e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-23 20:53:34 +0200

    filter users by name field

--- src/routes/v2/users.js
+++ src/routes/v2/users.js
@@ -11,7 +11,42 @@ const { userById } = require('../../middleware/user.middleware');
 
 router.get('/', requireSignIn, async (req, res) => {
   try {
-    let users = await User.find();
+    let firstNameFilter = req.query['firstName'];
+    if (firstNameFilter instanceof Array)
+      firstNameFilter = firstNameFilter[firstNameFilter.length - 1];
+    let lastNameFilter = req.query['lastName'];
+    if (lastNameFilter instanceof Array)
+      lastNameFilter = lastNameFilter[lastNameFilter.length - 1];
+    let middleNameFilter = req.query['middleName'];
+    if (middleNameFilter instanceof Array)
+      middleNameFilter = middleNameFilter[lastNameFilter.length - 1];
+
+    const filters = [];
+    if (firstNameFilter) {
+      filters.push({
+        'name.firstName': { $regex: firstNameFilter, $options: 'i' },
+      });
+    }
+    if (lastNameFilter) {
+      filters.push({
+        'name.lastName': { $regex: lastNameFilter, $options: 'i' },
+      });
+    }
+    if (middleNameFilter) {
+      filters.push({
+        'name.middleName': { $regex: middleNameFilter, $options: 'i' },
+      });
+    }
+
+    let users;
+    if (filters.length == 0) {
+      users = await User.find();
+    } else {
+      users = await User.find({
+        $or: filters,
+      });
+    }
+
     res.json(users);
   } catch (err) {
     res.json({ error: formatError(err) });
